5b235c234b5aa6c9ce3899229228210f408822e8
bethelhems <sisaybethelhem5@gmail.com>
2021-06-21 22:09:11 +0300

    Implement find_friends_count function

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -68,7 +68,8 @@ class TweetDfExtractor:
        followers_count = [tweet['user']['followers_count'] for tweet in self.tweets_list]
        return followers_count
     def find_friends_count(self)->list:
-        friends_count = 
+        friends_count =  friends_count = [tweet['user']['friends_count'] for tweet in self.tweets_list]
+        return friends_count
 
     def is_sensitive(self)->list:
         try:
