f242b93ad2738633cfcb547d8033ab333cfe3ded
bethelhems <sisaybethelhem5@gmail.com>
2021-06-21 22:35:24 +0300

    convert columns to numbers

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -42,11 +42,13 @@ class Clean_Tweets:
         convert columns like polarity, subjectivity, retweet_count
         favorite_count etc to numbers
         """
-        df['polarity'] = pd.----
+        df['polarity'] = pd.to_numeric(df["polarity"])
+        df["subjectivity"] = pd.to_numeric(df["subjectivity"])
+        df["retweet_count"] = pd.to_numeric(df["retweet_count"])
+        df["favorite_count"] = pd.to_numeric(df["favorite_count"])
         
-        ----
-        ----
         
+        return df
         return df
     
     def remove_non_english_tweets(self, df:pd.DataFrame)->pd.DataFrame:
